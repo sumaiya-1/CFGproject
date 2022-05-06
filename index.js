eea2a619e0b8ac6d1de9697f1397ee4b1ed19b1d
Sumaiya Ahmed <sumaiya157@hotmail.co.uk>
2022-05-06 19:39:18 +0100

    Updated search bar

---
+++ index.js
@@ -0,0 +1,9 @@
+// Open the full screen search box
+function openSearch() {
+    document.getElementById("mySearchBar").style.display = "block";
+  }
+  
+  // Close the full screen search box
+  function closeSearch() {
+    document.getElementById("mySearchBar").style.display = "none";
+  }
